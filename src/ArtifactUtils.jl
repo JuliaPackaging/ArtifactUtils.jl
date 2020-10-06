fa9e0edbb129ccdb02023578f108bec871c81655
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-10-06 20:11:25 +0200

    initial commit

--- src/ArtifactUtils.jl
+++ src/ArtifactUtils.jl
@@ -1,5 +1,47 @@
 module ArtifactUtils
 
-# Write your package code here.
+using Pkg.Artifacts
+using Pkg.PlatformEngines
+using Pkg.GitTools
+using SHA
+
+export add_artifact!
+
+function sha256sum(tarball_path)
+    return open(tarball_path, "r") do io
+        return bytes2hex(sha256(io))
+    end
+end
+
+
+function add_artifact!(
+    artifacts_toml::String,
+    name::String,
+    tarball_url::String;
+    clear=true,
+    options...,
+)
+    probe_platform_engines!()
+
+    tarball_path = download(tarball_url)
+    sha256 = sha256sum(tarball_path)
+
+    git_tree_sha1 = create_artifact() do artifact_dir
+        unpack(tarball_path, artifact_dir)
+    end
+
+    rm(tarball_path)
+    clear && remove_artifact(git_tree_sha1)
+
+    bind_artifact!(
+        artifacts_toml,
+        name,
+        git_tree_sha1;
+        download_info = [(tarball_url, sha256)],
+        options...,
+    )
+
+    return git_tree_sha1
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using ArtifactUtils
 using Test
+using TOML
 
 @testset "ArtifactUtils.jl" begin
-    # Write your tests here.
+    artifact_file = joinpath(mktempdir(), "Artifacts.toml")
+    add_artifact!(
+        artifact_file,
+        "JuliaMono",
+        "https://github.com/cormullion/juliamono/releases/download/v0.007/JuliaMono.tar.gz",
+        force=true,
+    )
+    artifacts = TOML.parsefile(artifact_file)
+    @test artifacts == Dict{String,Any}(
+        "JuliaMono" => Dict{String,Any}(
+            "git-tree-sha1" => "65279f9c8a3dd1e2cb654fdedbe8cd58889ae1bc",
+            "download" => Any[
+                Dict{String,Any}(
+                    "sha256" => "f1ab65231cda7981531398644a58fd5fde8f367b681e1b8e9c35d9b2aacfcb1c",
+                    "url" => "https://github.com/cormullion/juliamono/releases/download/v0.007/JuliaMono.tar.gz",
+                ),
+            ],
+        ),
+    )
+    rm(artifact_file)
 end
